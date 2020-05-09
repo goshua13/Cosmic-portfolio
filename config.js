957afa2738cd3708fd33cd7d7a69e9f8faac8996
goshua13 <joshuaaragon11@gmail.com>
2020-05-09 08:19:03 -0700

    first commit

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
+
 export default {
     bucket: {
-      slug: process.env.COSMIC_BUCKET || 'coffee-blog',
-      read_key: process.env.COSMIC_READ_KEY || '',
-      write_key: process.env.COSMIC_WRITE_KEY || '',
+      slug: process.env.COSMIC_BUCKET || 'the-brogrammer',
+      read_key: process.env.COSMIC_READ_KEY || 'geAHLGSH8lGP3K92l3ypL7BlGXy1tnhoLCvYdTni2Bk6xu3vVl',
+      write_key: process.env.COSMIC_WRITE_KEY || 'LIVj7fcavyBDFd2G6coChU3g32rjC83Vofgs0HusQfQpcKoqec',
       port: process.env.PORT || '',  
     }
   }
